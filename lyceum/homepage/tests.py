dcd1fbdee188036ff56d2dc380f333710c1e0c82
MichaIlia <159548675+MichaIlia@users.noreply.github.com>
2024-03-10 22:28:55 +0300

    Update tests.py

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -2,6 +2,9 @@ from http import HTTPStatus
 
 from django.test import Client, TestCase
 
+from rest_framework import status
+from rest_framework.test import APITestCase
+
 
 class StaticURLTests(TestCase):
     def test_homepage_endpoint(self):
@@ -11,3 +14,11 @@ class StaticURLTests(TestCase):
     def test_homepage_coffee(self):
         response = Client().get('/coffee/')
         self.assertEqual(response.status_code, 418)
+
+
+
+class ExampleTestCase(APITestCase):
+    def test_url_root(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertTrue(status.is_success(response.status_code))
